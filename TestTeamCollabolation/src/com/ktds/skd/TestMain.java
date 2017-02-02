cd8d55187b633783a352ec20ac87ec0e5346c114
Admin-PC\Admin <yarn18019@hanmail.net>
2017-02-02 10:49:23 +0900

    no message

--- TestTeamCollabolation/src/com/ktds/skd/TestMain.java
+++ TestTeamCollabolation/src/com/ktds/skd/TestMain.java
@@ -4,5 +4,7 @@ public class TestMain {
 	public static void main(String[] args) {
 		System.out.println("WelCome DragonBall");
 		System.out.println("이파일 지우고 작업하래여");
+		
+		System.out.println("송규동!!!!");
 	}
 }
