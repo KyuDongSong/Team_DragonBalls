bc542cbca29dea61458ad6869d0d0a06e70045f0
Admin-PC\Admin <Rheeyeji0321@gmail.com>
2017-02-02 10:56:18 +0900

    커밋

--- TestTeamCollabolation/src/com/ktds/skd/TestMain.java
+++ TestTeamCollabolation/src/com/ktds/skd/TestMain.java
@@ -5,10 +5,16 @@ public class TestMain {
 	public static void main(String[] args) {
 		
 		System.out.println("WelCome DragonBall");
+<<<<<<< HEAD
 		System.out.println("ÀÌ ÆÄÀÏÀ» »èÁ¦ÇÏ°í ¼ÂÆÃÇØÁÖ¼¼¿ë!!");
 		System.out.println("¼öÁ¤");
 		
 		System.out.println("ÀÌ¿¹Áö");
 		
+=======
+		System.out.println("ì´íŒŒì¼ ì§€ìš°ê³  ìž‘ì—…í•˜ëž˜ì—¬");
+		
+		System.out.println("ê¹€ì •ì§„");
+>>>>>>> a8d6621acfec973482badda1a51f858f17bc977a
 	}
 }
